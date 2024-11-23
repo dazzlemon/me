636bacb95d1c2f9d30997a01444481af32afad77
dazzlemon <daanill13470@gmail.com>
2024-11-23 19:34:30 +0200

    update analysis_options

--- lib/base/menu.dart
+++ lib/base/menu.dart
@@ -28,5 +28,6 @@ Widget menu(BuildContext context) => ColoredBox(
 
 extension IterableIntersperse<T> on Iterable<T> {
   Iterable<T> intersperse(T t) =>
+      // ignore: no_magic_number
       isEmpty ? [] : expand((e) => [e, t]).take(length * 2 - 1);
 }
