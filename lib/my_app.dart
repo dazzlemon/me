d1bcf086613561ee8a959a6e9659d84cfa627a89
dazzlemon <daanill13470@gmail.com>
2024-11-23 13:54:54 +0200

    add functional_widget

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:me/my_home_page.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+part 'my_app.g.dart';
 
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      );
-}
+@swidget
+Widget myApp() => MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
