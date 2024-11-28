efd5175b29dd1469fb58484fac72f9e76a5389dd
dazzlemon <daanill13470@gmail.com>
2024-11-28 22:26:33 +0200

    more styled dropdown

--- lib/base/base_dropdown_menu.dart
+++ lib/base/base_dropdown_menu.dart
@@ -15,47 +15,52 @@ Widget baseDropdownMenu<T>(
   required List<T> values,
 }) {
   final theme = ref.watch(themeNotifierProvider).values;
-  final color = theme[AppColor.text] ?? Colors.transparent;
+  final textColor = theme[AppColor.text] ?? Colors.transparent;
+  final backgroundColor = theme[AppColor.accent] ?? Colors.transparent;
 
-  return DropdownMenu<T>(
-    menuStyle: MenuStyle(
-      backgroundColor: const WidgetStatePropertyAll(Colors.transparent),
-      surfaceTintColor: const WidgetStatePropertyAll(Colors.transparent),
-      shadowColor: const WidgetStatePropertyAll(Colors.transparent),
-      elevation: const WidgetStatePropertyAll(0),
-      padding: const WidgetStatePropertyAll(EdgeInsets.zero),
-      side: WidgetStatePropertyAll(BorderSide(color: color)),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: color,
-        ),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: color,
-        ),
+  return Theme(
+    data: ThemeData(
+      textSelectionTheme: TextSelectionThemeData(
+        cursorColor: textColor,
+        selectionColor: textColor,
       ),
     ),
-    textStyle: TextStyle(
-      color: theme[AppColor.text] ?? Colors.transparent,
-    ),
-    initialSelection: initialSelection,
-    onSelected: (t) => t == null ? null : onSelected(t),
-    dropdownMenuEntries: values
-        .map(
-          (e) => DropdownMenuEntry(
-            value: e,
-            label: valueToString(e),
-            style: ButtonStyle(
-              foregroundColor: WidgetStatePropertyAll(
-                theme[AppColor.text] ?? Colors.transparent,
+    child: DropdownMenu<T>(
+      menuStyle: MenuStyle(
+        backgroundColor: WidgetStatePropertyAll(backgroundColor),
+        surfaceTintColor: const WidgetStatePropertyAll(Colors.transparent),
+        shadowColor: const WidgetStatePropertyAll(Colors.transparent),
+        elevation: const WidgetStatePropertyAll(0),
+        padding: const WidgetStatePropertyAll(EdgeInsets.zero),
+        shape: const WidgetStatePropertyAll(ContinuousRectangleBorder()),
+        visualDensity: VisualDensity.standard,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        suffixIconColor: textColor,
+        filled: true,
+        fillColor: backgroundColor,
+        errorBorder: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        focusedErrorBorder: InputBorder.none,
+        disabledBorder: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        border: InputBorder.none,
+      ),
+      textStyle: TextStyle(color: textColor),
+      initialSelection: initialSelection,
+      onSelected: (t) => t == null ? null : onSelected(t),
+      dropdownMenuEntries: values
+          .map(
+            (e) => DropdownMenuEntry(
+              value: e,
+              label: valueToString(e),
+              style: MenuItemButton.styleFrom(
+                foregroundColor: theme[AppColor.text] ?? Colors.transparent,
+                shape: const ContinuousRectangleBorder(),
               ),
-              backgroundColor: const WidgetStatePropertyAll(Colors.transparent),
             ),
-          ),
-        )
-        .toList(),
+          )
+          .toList(),
+    ),
   );
 }
