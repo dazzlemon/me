33fc2f80fd53743c5387b7b4fa80b9840f2a909e
dazzlemon <daanill13470@gmail.com>
2024-12-01 00:19:01 +0200

    menu

--- lib/base/scaffold.dart
+++ lib/base/scaffold.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
-import 'package:me/base/menu.dart';
+import 'package:me/base/menu/menu.dart';
 import 'package:me/theme/model/app_color.dart';
 import 'package:me/theme/state/theme.dart';
 
---
+++ lib/blog/body.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:me/base/wip_body.dart';
+
+part 'body.g.dart';
+
+@swidget
+Widget body() => const WipBody();
